a1706aef706f9c6517321277cc948e8f293f3b55
adieltapari <adiel.tapari@gmail.com>
2021-09-13 20:04:17 -0300

    desafío03

---
+++ desafíos/03/js/main.js
@@ -0,0 +1,9 @@
+function numero() {
+    let numero = prompt(" ¡ Ingresa la cantidad de holas que desea repetir !");
+
+    for (let i = 1; i <= numero; i++) {
+        alert("HOLA" + " " + i);
+    }
+}
+
+
